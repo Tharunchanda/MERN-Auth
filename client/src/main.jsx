7f22402518618fea6daec70a2fff0a5b57a75acc
THARUN CHANDA <b23121@students.iitmandi.ac.in>
2025-01-02 11:18:36 +0530

    working

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import { AppContextProvider } from './context/AppContext.jsx'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { AppContextProvider } from './context/AppContext.jsx';
+import App from './App.jsx';
+import './index.css'; // Ensure this import is present
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <AppContextProvider>
       <App />
     </AppContextProvider>
-  </BrowserRouter>,
-)
+  </BrowserRouter>
+);
